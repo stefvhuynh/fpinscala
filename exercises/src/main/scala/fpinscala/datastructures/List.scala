543b1f04d3d80847ecbf4e6057b9dc3505313cc5
Stefan Huynh <huynh.stefan@gmail.com>
2015-04-12 23:03:41 -0400

    Finish tail exercise

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -49,8 +49,10 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("cannot return tail of empty list")
+    case Cons(_, t) => t
+  }
 
   def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
 
