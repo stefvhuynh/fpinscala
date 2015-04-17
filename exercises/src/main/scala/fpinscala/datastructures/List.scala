a262fb6490c5be1024f1e331d893167ca121823b
Stefan Huynh <huynh.stefan@gmail.com>
2015-04-16 23:37:46 -0400

    Finish length exercise

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -78,7 +78,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(x, xs) => Cons(x, init(xs))
   }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, y) => y + 1)
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
 
